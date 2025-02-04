c4d286b316e27867e0db8ecebfe2aeb34f1de3fb
ramesh1918s <shivram915454@gmail.com>
2025-02-04 17:04:13 +0530

    add variable.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "vpc_cidr" {}
+variable "subnet1_cidr" {}
+variable "subnet2_cidr" {}
+variable "subnet3_cidr" {}
+variable "ami_id" {}
+variable "instance_type" {}
+# variable "public_key" {}
